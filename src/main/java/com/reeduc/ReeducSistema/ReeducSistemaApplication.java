9c5963c149402e157cf1fa3dd3d49ec38b2170a5
Davi Lins <davi.carnauba@gmail.com>
2025-04-08 13:36:07 -0300

    first commit

---
+++ src/main/java/com/reeduc/ReeducSistema/ReeducSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.reeduc.ReeducSistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReeducSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReeducSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reeduc/ReeducSistema/ReeducSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reeduc.ReeducSistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReeducSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
