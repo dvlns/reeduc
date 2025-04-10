66bd186f2d2aa953c0a826403e126dd756e700ac
Davi Lins <davi.carnauba@gmail.com>
2025-04-10 11:22:12 -0300

    feat: create user entity

---
+++ src/main/java/com/reeduc/ReeducSistema/model/User.java
@@ -0,0 +1,19 @@
+package com.reeduc.ReeducSistema.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String password;
+}
